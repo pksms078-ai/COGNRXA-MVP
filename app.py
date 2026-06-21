e2a7dec3b3a9a1cfcebf7ca0908ab619a9a760a8
Prabin Kumar <pksms078@gmail.com>
2026-06-21 18:15:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,3 +11,49 @@ Load AI engine.
 Handle API requests.
 ROUTES
 Plain text
+
+Python
+from flask import Blueprint
+
+analysis_bp = Blueprint(
+    "analysis",
+    __name__
+)
+
+@analysis_bp.route("/analyze")
+def analyze():
+
+    return {
+        "message":"Analysis endpoint"
+    }
+routes/insight_routes.py
+Python
+from flask import Blueprint
+
+insight_bp = Blueprint(
+    "insight",
+    __name__
+)
+
+@insight_bp.route("/insights")
+def insights():
+
+    return {
+        "message":"Insights endpoint"
+    }
+routes/decision_routes.py
+Python
+from flask import Blueprint
+
+decision_bp = Blueprint(
+    "decision",
+    __name__
+)
+
+@decision_bp.route("/decisions")
+def decisions():
+
+    return {
+        "message":"Decision endpoint"
+    }
+
