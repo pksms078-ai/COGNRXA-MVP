44b65b83997e3113d210e525243746b4cdc48c26
Prabin Kumar <pksms078@gmail.com>
2026-06-21 18:32:30 +0530

    Create decision.py

---
+++ decision.py
@@ -0,0 +1,12 @@
+Python
+class Decision:
+
+    def __init__(
+        self,
+        recommendation,
+        risk_level
+    ):
+
+        self.recommendation = recommendation
+
+        self.risk_level = risk_level
